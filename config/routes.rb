5c870b40b591b9463653a3e2b59f62625823f72b
McProdigy <ikopitm@gmail.com>
2021-04-09 14:17:04 +0300

    add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root 'pages#home'
   get 'help' => 'pages#help'
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
